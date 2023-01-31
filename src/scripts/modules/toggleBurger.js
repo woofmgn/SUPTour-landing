c4a5d17c3975057118b6eb393e7374342568db44
Denis Vakulenko <101655241+woofmgn@users.noreply.github.com>
2023-01-31 15:34:23 +0500

    refactor: burger menu

--- src/scripts/modules/toggleBurger.js
+++ src/scripts/modules/toggleBurger.js
@@ -1,9 +1,13 @@
-const burgerOverlay = document.querySelector('.header__burger-menu');
-const burgerMenu = document.querySelector('.header__burger-wrapper');
+const burgerOverlay = document.querySelector('.navigation__burger-menu');
+const burgerMenu = document.querySelector(
+  '.navigation__item-list_type_burger-wrapper'
+);
 const menuIcon = document.querySelector('.header__burger-button');
 
 export const toggleBurger = () => {
-  burgerOverlay.classList.toggle('header__burger-menu_active');
-  burgerMenu.classList.toggle('header__burger-wrapper_active');
+  burgerOverlay.classList.toggle('navigation__burger-menu_active');
+  burgerMenu.classList.toggle(
+    'navigation__item-list_type_burger-wrapper_active'
+  );
   menuIcon.classList.toggle('active');
 };
