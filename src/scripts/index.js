901c764b1c2d136689195df1dfd7782e3de24c06
Denis Vakulenko <101655241+woofmgn@users.noreply.github.com>
2023-02-05 12:15:47 +0500

    feat: added bahavior from scroll

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,8 +4,13 @@ import { swiper } from './modules/swiperSlider';
 import { toggleBurger } from './modules/toggleBurger';
 
 const burgerButton = document.querySelector('.header__burger-button');
+const burgerLink = document.querySelectorAll('.navigation__link_type_burger');
 
 swiper.slideNext();
 initMask();
 
 burgerButton.addEventListener('click', toggleBurger);
+
+burgerLink.forEach((link) => {
+  link.addEventListener('click', toggleBurger);
+});
